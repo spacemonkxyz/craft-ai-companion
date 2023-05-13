7eda4e937625968d2c3b9ae5b46e46aad4a75913
Stefan Worzalla <stefan.worzalla@gmail.com>
2023-05-13 14:38:20 +0700

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Release Notes for craft-ai-companion
+
+## 1.0.0
+- Initial release
---
+++ src/models/Settings.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace spacemonk\ai_companion\models;
+
+use Craft;
+use craft\base\Model;
+
+/**
+ * craft-ai-companion settings
+ */
+class Settings extends Model
+{
+}

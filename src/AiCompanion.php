1ae2d5d94d979397ffbedbb7e49d652a5a24e31b
Stefan Worzalla <stefan.worzalla@gmail.com>
2023-05-13 21:49:10 +0700

    Add logo and api key settings

--- src/AiCompanion.php
+++ src/AiCompanion.php
@@ -6,6 +6,11 @@ use Craft;
 use craft\base\Model;
 use craft\base\Plugin;
 use spacemonk\ai_companion\models\Settings;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+use yii\base\Exception;
+use yii\base\InvalidConfigException;
 
 /**
  * craft-ai-companion plugin
@@ -20,6 +25,7 @@ class AiCompanion extends Plugin
 {
     public string $schemaVersion = '1.0.0';
     public bool $hasCpSettings = true;
+    public bool $hasCpSection = true;
 
     public static function config(): array
     {
@@ -30,27 +36,45 @@ class AiCompanion extends Plugin
         ];
     }
 
-    public function init()
+    public function init(): void
     {
         parent::init();
 
         // Defer most setup tasks until Craft is fully initialized
         Craft::$app->onInit(function() {
             $this->attachEventHandlers();
-            // ...
+            $this->_registerControlPanelPages();
         });
     }
 
+    /**
+     * @throws InvalidConfigException
+     */
     protected function createSettingsModel(): ?Model
     {
         return Craft::createObject(Settings::class);
     }
 
+    /**
+     * @inheritDoc
+     * @throws SyntaxError
+     * @throws RuntimeError
+     * @throws Exception
+     * @throws LoaderError
+     */
     protected function settingsHtml(): ?string
     {
-        return Craft::$app->view->renderTemplate('craft-ai-companion/_settings.twig', [
+        // Get and pre-validate the settings
+        $settings = $this->getSettings();
+        $settings->validate();
+
+        // Get the settings that are being defined by the config file
+        $overrides = Craft::$app->getConfig()->getConfigFromFile(strtolower($this->handle));
+
+        return Craft::$app->view->renderTemplate($this->handle . '/_settings.twig', [
             'plugin' => $this,
-            'settings' => $this->getSettings(),
+            'settings' => $settings,
+            'overrides' => array_keys($overrides)
         ]);
     }
 
